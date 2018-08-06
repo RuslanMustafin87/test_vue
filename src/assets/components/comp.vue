81c9a8d643e4b31438f165bbc59e08c269927299
Ruslan Mustafin <ruslanmust87@gmail.com>
2018-08-07 02:13:51 +0500

    com

---
+++ src/assets/components/comp.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+        h1.title {{title}}
+</template>
+
+<script>
+export default {
+        data: function(){
+            return {
+                title: 'Hello world!!!'
+            }
+    }
+}
+</script>
+
+<style>
+.title{
+    color:red;
+}
+</style>
