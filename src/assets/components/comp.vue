dfb3c0e70affcc5d24e4ec3e148e0503f5806760
Ruslan Mustafin <ruslanmust87@gmail.com>
2018-08-22 01:07:51 +0500

    com_2

--- src/assets/components/comp.vue
+++ src/assets/components/comp.vue
@@ -1,19 +1,40 @@
 <template lang="pug">
-        h1.title {{title}}
+        //- .slider
+        //-     .slider__container
+        //-         ul.slider__items
+        //-             -   for (var i = 1;i <= 3; i++)
+        //-                     li.slider__item(class=`slider__item-${i}`)
+        div
+            
+            form.form
+                input(type='text' v-model='text')
+                .text text: {{text}}
+                br
+                <input type='radio' class='radio__input' id='radio_1' name='radio' value='Yes' v-model='radio'>
+                label(class='radio__label radio__label-1' for='radio_1')
+               
+                <input type='radio' class='radio__input' id='radio_2' name='radio' value='No' v-model='radio'>
+                label(class='radio__label radio__label-2' for='radio_2')
+                span {{radio}}
+                select(v-model='select')
+                    option() 1
+                    option 2
+                    option 3
+                span {{select}}
 </template>
 
 <script>
 export default {
-        data: function(){
+        data: ()=>{
             return {
-                title: 'Hello world!!!'
+                title: 'Hello world!!!',
+                text: 'tuyrert',
+                radio: 'Yes',
+                select: []
             }
     }
 }
 </script>
 
-<style>
-.title{
-    color:red;
-}
+<style lang="scss" src='./style.scss' scoped>
 </style>
