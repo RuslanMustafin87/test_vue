769bfd63f42edd001ee23d14c012713edb2768bb
Ruslan Mustafin <ruslanmust87@gmail.com>
2018-09-06 17:19:47 +0500

    com_9

--- src/assets/components/item/index.vue
+++ src/assets/components/item/index.vue
@@ -8,29 +8,44 @@
         br
         ul.list
             li.list__item(v-for='(todo,index) in todos')
-                
+                todo(@remove-item='removeTodo' 
+                    @change-item='changeItem' 
+                    :titleItem='todo' 
+                    :indexItem='index')
 </template>
 
 <script>
+import todo from '../todo/index';
+import {$eventBus} from '../../../main.js';
+
 export default {
     data(){
         return {
             todos: [],
+            newTodo: '',
         }
     },
-    props: {
-        titleItem: {
-            type: String,
-            default: 'Title'
-        },
-        indexItem: {
-            type: Number,
-        }
-    },
+    // created(){
+    //     $eventBus.$on('remove-item',(id)=>{
+    //         this.todos.splice(index, 1)
+    //     }),
+    //     $eventBus.$on('change-item',(titleChange,id)=>{
+    //         this.todos.splice(index, 1, titleChange)
+    //     })
+    // },
     methods: {
         addTodo(){
-            this.todos.push()
+            this.todos.push(this.newTodo)
+        },
+        removeTodo(index){
+            this.todos.splice(index, 1)
+        },
+        changeItem(titleChange, index){
+            this.todos.splice(index, 1, titleChange)
         }
+    },
+    components: {
+        todo
     }
 }
 </script>
--- src/assets/components/todo/index.vue
+++ src/assets/components/todo/index.vue
@@ -1,17 +1,44 @@
 <template lang="pug">
     h2 {{indexItem + 1}}. {{titleItem}}
         hr
-        button.btn.btn__delete(@click='removeItem(index)') Удалить
-        button.btn.btn__change Изменить
+        input(type='text' v-if='showInputChange' v-model='titleChange' @keydown.enter='changeTitle(indexItem)')
+        br
+        button.btn.btn__delete(@click='removeItem(indexItem)') Удалить
+        button.btn.btn__change(@click='showItem') Изменить
         router-link(class='btn btn__view' tag='button' :to='{name: `view`, params: {title: titleItem}}') Посмотреть
 </template>
 
 <script>
+
+import {$eventBus} from '../../../main.js';
+
 export default {
+  data(){
+    return {
+      showInputChange: false,
+      titleChange: ''
+    }
+  },
+  props: {
+        titleItem: {
+            type: String,
+            default: 'Title'
+        },
+        indexItem: {
+            type: Number,
+        }
+    },
   methods: {
     removeItem(id){
-            this.$emit('remove-item', id)
+      $eventBus.$emit('remove-item', id)
         },
+    showItem(){
+      this.showInputChange = true;
+    },
+    changeTitle(id){
+      $eventBus.$emit('change-item', this.titleChange, id);
+      this.showInputChange = false;
+    }
   }
 }
 </script>
